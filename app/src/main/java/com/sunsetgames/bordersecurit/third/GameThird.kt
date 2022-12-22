e98002322df95b2980396e9d564ad60965e4541b
Marina <marina@Marinas-MacBook-Pro.local>
2022-12-22 14:33:01 +0300

    kk

--- app/src/main/java/com/sunsetgames/bordersecurit/third/GameThird.kt
+++ app/src/main/java/com/sunsetgames/bordersecurit/third/GameThird.kt
@@ -19,12 +19,6 @@ class GameThird : Fragment() {
     private val yyyyy
         get() = dddd ?: throw RuntimeException("FragmentGameThirdBinding = null")
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
